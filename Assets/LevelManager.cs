4d0ebc7efb34a931a172b96a12eafa575fd9f387
jamiex304 <jamiex304@hotmail.com>
2017-02-28 22:45:45 +0000

    Adding Button Funtions
    Added a start button to switch between levels & debug prints for testing
    in console

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName){
+		Debug.Log("Level Load Requested for: " + levelName);
+		Application.LoadLevel(levelName); //Loads a level
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Quit Requested for Game");
+	}
+	
+}
